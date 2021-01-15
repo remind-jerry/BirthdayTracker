8b3fe14c07dd7a16e2d2937f193fd9536e4ea1c6
remind-jerry <76900235+remind-jerry@users.noreply.github.com>
2021-01-15 19:59:46 +0400

    Работает добавление записей в таблицу

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -6,10 +6,7 @@
 //
 
 import UIKit
-
-protocol AddBirthdayViewControllerDelegate {
-    func addBirthdayViewController(_ addBirthdayViewController: AddBirthdayViewController, didAddBirthday birthday: Birthday)
-}
+import CoreData
 
 class AddBirthdayViewController: UIViewController {
     @IBOutlet var firstNameTextField: UITextField!
@@ -18,31 +15,42 @@ class AddBirthdayViewController: UIViewController {
     
     private var firstName = ""
     private var lastName = ""
-    
-    var delegate: AddBirthdayViewControllerDelegate?
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         birthdatePicker.maximumDate = Date()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func saveTapped( _ sender: UIBarButtonItem) {
         print("Нажата кнопка сохранения.")
         firstName = firstNameTextField.text!.count > 0 ? firstNameTextField.text! : "no_name"
         lastName = lastNameTextField.text!.count > 0 ? lastNameTextField.text! : "no_last_name"
-//        print("Меня зовут \(firstName), моя фамилия \(lastName).")
+
         let birthdate = birthdatePicker.date
-        let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
-        print("Создана запись о днерождения!")
-        print("Имя: \(newBirthday.firstName)")
-        print("Фамилия: \(newBirthday.lastName)")
-        print("День рождения: \(newBirthday.birthdate)")
-        delegate?.addBirthdayViewController(self, didAddBirthday: Birthday(firstName: firstName, lastName: lastName, birthdate: birthdatePicker.date))
-        dismiss(animated: true)
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newBirthday = Birthday(context: context)
+        
+        newBirthday.firstName = firstName
+        newBirthday.lastName = lastName
+        newBirthday.birthdate = birthdate as Date?
+        newBirthday.birthdayId = UUID().uuidString
+        if let uniqueId = newBirthday.birthdayId {
+            print("birthdayId: \(uniqueId)")
+        }
+        do {
+            try context.save()
+        } catch let error {
+            print("Не удалось сохранить из-за ошибки \(error).")
+        }
+        print("Создана запись о дне рождения!")
+        print("Имя: \(newBirthday.firstName ?? "")")
+        print("Фамилия: \(newBirthday.lastName ?? "")")
+        print("День рождения: \(newBirthday.birthdate ?? Date())")
+        navigationController?.popViewController(animated: true)
     }
     
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        navigationController?.popViewController(animated: true)
     }
 }
